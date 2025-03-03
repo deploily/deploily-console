a1319b8fdeb32a31eeee736c06664562b60fdaf3
bentayeb dounia <dounia.bentayeb@transformatek.dz>
2025-03-03 13:38:13 +0000

    add support ticket url

--- deploily-console/src/deploilyWebsiteUrls.ts
+++ deploily-console/src/deploilyWebsiteUrls.ts
@@ -5,6 +5,7 @@ export const deploilyApiUrls = {
   CART_URL: `${process.env.API_BASE_URL}/cart/`,
   CART_LINE_URL: `${process.env.API_BASE_URL}/cartline/`,
   MY_FAVORITE_URL: `${process.env.API_BASE_URL}/myfavorites/`,
+  SUPPORT_TICKET_URL: `${process.env.API_BASE_URL}/supportticket/`,
   LOGIN_URL: `${process.env.API_BASE_URL}/security/login/`,
   GENERATE_TOKEN_URL:` ${ process.env.API_BASE_URL }/consumer/cart-line/`
 };
