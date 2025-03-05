991610a5435eabfa5aa74812edfa765960b5fa88
bentayeb dounia <dounia.bentayeb@transformatek.dz>
2025-03-05 13:41:11 +0000

    post suport ticket / fixes

--- deploily-console/src/app/[locale]/(auth)/portal/supportTicket/add/components/createSupportTicket.tsx
+++ deploily-console/src/app/[locale]/(auth)/portal/supportTicket/add/components/createSupportTicket.tsx
@@ -1,17 +1,73 @@
 // import { useI18n } from "../../../../../../../locales/client";
 
 import { Plus } from "@phosphor-icons/react";
-import { Button, Input, Row, Select, Typography, Upload } from "antd";
+import { Button, Form, Input, message, Row, Select, Typography, Upload, UploadFile, UploadProps } from "antd";
 import TextArea from "antd/es/input/TextArea";
 import Title from "antd/es/typography/Title";
 import { useScopedI18n } from "../../../../../../../../locales/client";
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { UploadOutlined } from '@ant-design/icons';
+import { useAppDispatch } from "@/lib/hook";
+import { fetchMyServices } from "@/lib/features/myService/myServiceThunks";
+import { useMyService } from "@/lib/features/myService/myServiceSelectors";
+import { myServiceInterface } from "@/lib/features/myService/myServiceInterface";
+import { postSupportTicket } from "@/lib/features/support-ticket/supportTicketThanks";
+import { useSupportTicket } from "@/lib/features/support-ticket/supportTicketSelector";
+const { Option } = Select;
 
 export default function CreateSupportTecket() {
     const t = useScopedI18n('createSupportTicket')
-    
+    const [form] = Form.useForm();
+    const dispatch = useAppDispatch();
+    const { myServiceResponse, myServiceLoading, myServiceLoadingError } = useMyService()
+    const { addSupportTicketSuccess, addSupportTicketError } = useSupportTicket()
+    // const [file, setFile] = useState(null);
+    const [fileList, setFileList] = useState<UploadFile[]>()
+    const [messageApi] = message.useMessage();
+
+    useEffect(() => {
+        dispatch(fetchMyServices());
+        if (addSupportTicketSuccess) {
+            messageApi.open({
+                type: 'success',
+                content: 'Support ticket created successfully!',
+            });
+        }
+
+        if (addSupportTicketError) {
+            messageApi.open({
+                type: 'error',
+                content: 'Support ticket created successfully!',
+            });
+
+        }
+    }, [addSupportTicketSuccess, addSupportTicketError]);
+
+    const handleChange: UploadProps['onChange'] = ({ fileList: newFile }) =>
+        setFileList(newFile);
+    const onFinish = (values: any) => {
+        console.log(values);
+
+        console.log("file=== ", fileList);
+
+        dispatch(postSupportTicket({
+            my_service_id: values.service,
+            title: values.subject,
+            description: values.description,
+            image: (fileList && fileList.length > 0) ? fileList[0].thumbUrl : null,
+            status: "open"
+        })).then(() => {
+            setFileList([]);
+            form.resetFields();
+        })
+
+
+    };
+
+
+
+
     return (
         <>
             <Title level={3} style={{ fontWeight: 700, color: '#ffff' }}>
@@ -22,73 +78,81 @@ export default function CreateSupportTecket() {
             <Typography style={{ marginBlock: 30, fontSize: 16 }}>
                 {t("typography")}
             </Typography>
+            <Form
+                form={form}
+                name="control-hooks"
+                onFinish={onFinish}
+            >
+                <Form.Item name="service" rules={[{ required: true }]}>
+                    <Select
+                        allowClear
+                        style={{ width: "100%", height: 40 }}
+                        placeholder={t("selectService")}
 
-            <Select
-                style={{ width: "100%", height: 40 }}
-                showSearch
-                placeholder={t("selectService")}
-                optionFilterProp="label"
-                filterSort={(optionA, optionB) =>
-                    (optionA?.label ?? '').toLowerCase().localeCompare((optionB?.label ?? '').toLowerCase())
-                }
-                options={[
-                    {
-                        value: '1',
-                        label: 'Service 1',
-                    },
-                    {
-                        value: '2',
-                        label: 'Service 2',
-                    },
-                    {
-                        value: '3',
-                        label: 'Service 3',
-                    },
+                    >
+                        {myServiceResponse !== undefined ? myServiceResponse?.result?.map((value: myServiceInterface) => (
+                            <Option key={value.id} value={value.id}>{value.name}</Option>
+                        )) : null}
 
-                ]}
+                    </Select>
 
-            />
-            <Input style={{ marginBlock: 20, height: 40, borderRadius: 0 }} placeholder={t("subject")} allowClear />
-            <TextArea
-                style={{ borderRadius: 0 }}
-                placeholder={t("description")}
-                autoSize={{ minRows: 5, maxRows: 10 }}
-            />
-            <div style={{ marginBlock: 20, }}>
-                <Upload style={{ border: "none" }}
-                    action="https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload"
-                    listType="picture"
-                    maxCount={1}
-                // defaultFileList={fileList}
-                >
-                    <Button style={{
-                        color: "#fff",
-                        backgroundColor: "#D85912",
-                        border: "none",
-                        paddingInline: 10,
-                    }}>
-                        <UploadOutlined />
-                        {t('uploadFile')}
-                    </Button>
-                </Upload>
-            </div>
-            <Row style={{ display: "flex", justifyContent: "end" }} >
-                <Button
-                    style={{
-                        color: "#ffff",
-                        backgroundColor: "#5394CC",
-                        padding: 10,
-                        marginTop: 20,
-                        borderRadius: 25,
-                        fontSize: 15,
-                        height: 45
-                    }}
-                    onClick={() => { }}
-                >
-                    <Plus size={20} style={{ color: "rgba(220, 233, 245, 0.88)" }} />
-                    {t("createTicket")}
-                </Button>
-            </Row>
+                </Form.Item>
+                <Form.Item name="subject" rules={[{ required: true }]}>
+                    <Input
+                        allowClear
+                        placeholder={t("subject")}
+                        style={{ marginBlock: 20, height: 40, borderRadius: 0 }} />
+                </Form.Item>
+                <Form.Item name="description" rules={[{ required: true }]}>
+                    <TextArea
+                        style={{ borderRadius: 0 }}
+                        placeholder={t("description")}
+                        autoSize={{ minRows: 5, maxRows: 10 }}
+
+                    />
+                </Form.Item>
+                <Form.Item name="image" rules={[{ required: true }]}>
+                    <div style={{ marginBlock: 20, }}>
+                        <Upload style={{ border: "none" }}
+                            accept="image/png, image/jpeg"
+                            listType="picture"
+                            maxCount={1}
+                            fileList={fileList}
+                            beforeUpload={() => false}
+                            onChange={handleChange}
+                        >
+                            <Button style={{
+                                color: "#fff",
+                                backgroundColor: "#D85912",
+                                border: "none",
+                                paddingInline: 10,
+                            }}>
+                                <UploadOutlined />
+                                {t('uploadFile')}
+                            </Button>
+                        </Upload>
+                    </div>
+                </Form.Item>
+                <Form.Item>
+                    <Row style={{ display: "flex", justifyContent: "end" }} >
+                        <Button htmlType="submit"
+                            style={{
+                                color: "#ffff",
+                                backgroundColor: "#5394CC",
+                                padding: 10,
+                                marginTop: 20,
+                                borderRadius: 25,
+                                fontSize: 15,
+                                height: 45
+                            }}
+                            onClick={() => { }}
+                        >
+                            <Plus size={20} style={{ color: "rgba(220, 233, 245, 0.88)" }} />
+                            {t("createTicket")}
+                        </Button>
+                    </Row>
+                </Form.Item>
+            </Form>
         </>
     )
 }
--- deploily-console/src/app/[locale]/(auth)/portal/supportTicket/add/page.tsx
+++ deploily-console/src/app/[locale]/(auth)/portal/supportTicket/add/page.tsx
@@ -8,6 +8,7 @@ export default function Page() {
   return (
     <Content style={{padding: 20, color: "#27292E", marginInline:20}}>
       <CreateSupportTecket/>
+
     </Content>
   );
 }
