a6b3fb94ea5e1cda9be354d9f8eefb35ef3963b8
bentayeb dounia <dounia.bentayeb@transformatek.dz>
2025-03-05 13:37:34 +0000

    add url

--- deploily-console/src/deploilyWebsiteUrls.ts
+++ deploily-console/src/deploilyWebsiteUrls.ts
@@ -2,10 +2,11 @@ export const BASE_URL = `${process.env.API_BASE_URL}`;
 export const Admin_URL = `https://admin.deploily.cloud/static/uploads/`;
 export const deploilyApiUrls = {
   SERVICE_URL: `${process.env.API_BASE_URL}/service/`,
+  My_SERVICE_URL: `${process.env.API_BASE_URL}/my-service/`,
   CART_URL: `${process.env.API_BASE_URL}/cart/`,
   CART_LINE_URL: `${process.env.API_BASE_URL}/cartline/`,
   MY_FAVORITE_URL: `${process.env.API_BASE_URL}/myfavorites/`,
-  SUPPORT_TICKET_URL: `${process.env.API_BASE_URL}/supportticket/`,
+  SUPPORT_TICKET_URL: `${process.env.API_BASE_URL}/support-ticket/`,
   LOGIN_URL: `${process.env.API_BASE_URL}/security/login/`,
   GENERATE_TOKEN_URL:` ${ process.env.API_BASE_URL }/consumer/cart-line/`
 };
