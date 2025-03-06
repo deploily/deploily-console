33bbd80e2b565a1a4f23f11f99e75f4a6638a76e
ziadi kheira <kheira.ziadi@transformatek.dz>
2025-03-06 12:22:11 +0000

    Test generate token / fix state / display curl.

--- deploily-console/src/app/[locale]/(auth)/portal/my-services/components/serviceParameterComponent.tsx
+++ deploily-console/src/app/[locale]/(auth)/portal/my-services/components/serviceParameterComponent.tsx
@@ -3,89 +3,71 @@ import { generateTokenThunk } from "@/lib/features/cartLine/cartLineThunks";
 import { Col, Row, Button, Input, message, Tooltip } from "antd";
 import { useEffect } from "react";
 import { Copy } from "@phosphor-icons/react";
-import { fetchServiceParameters } from "@/lib/features/serviceParameters/serviceParametersThunks";
 import { useAppDispatch } from "@/lib/hook";
-import { useServiceParameters } from "@/lib/features/serviceParameters/serviceParametersSelectors";
 import { useI18n } from "../../../../../../../locales/client";
 import { fetchServiceParametersValues } from "@/lib/features/myServiceParameterValues/myServiceParameterValuesThunks";
 import { useServiceParametersValues } from "@/lib/features/myServiceParameterValues/myServiceParameterValuesSelectors";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { useCartLine } from "@/lib/features/cartLine/cartLineSelectors";
 
-interface ServiceParameterProps {
-    cartLine_id: string;
-    service_id: number;
-}
 
-export default function ServiceParameterComponent({ cartLine_id, service_id }: ServiceParameterProps) {
+export default function ServiceParameterComponent({ cartLine_id }: { cartLine_id: string }) {
+
     const dispatch = useAppDispatch();
-    const { serviceParametersList } = useServiceParameters();
     const { serviceParameterValuesList } = useServiceParametersValues();
+    const { currentCartLine } = useCartLine();
+    const { generatedToken } = useCartLine()
+    useEffect(() => {
+        dispatch(fetchServiceParametersValues(cartLine_id));
+    }, [generatedToken]);
+
     const t = useI18n();
 
-    useEffect(() => {
-        dispatch(fetchServiceParameters(service_id));
-        dispatch(fetchServiceParametersValues(cartLine_id));
-    }, [service_id, cartLine_id]);
-
-    const generateApiKey = () => {
-        dispatch(generateTokenThunk(cartLine_id));
-    };
+    const generateApiKey = () => { dispatch(generateTokenThunk(cartLine_id)) }
 
     const handleCopy = (value: string) => {
-        if (value) {
-            navigator.clipboard.writeText(value);
-            message.success("Copied to clipboard!");
-        }
+        navigator.clipboard.writeText(value);
+        message.success("Copied to clipboard!");
     };
 
     return (
         <>
-            {serviceParametersList?.result.map((parameter) => {
-                const parameterValue = serviceParameterValuesList?.result.find(param => param.id === parameter.id)?.value || "";
-
-                return (
-                    <div key={parameter.id}>
-                        {parameter.type !== 'token' ? (
+            {serviceParameterValuesList != undefined && serviceParameterValuesList?.result != undefined &&
+                <>
+                    {serviceParameterValuesList?.result?.map((parameterValue) => (
+                        <div key={parameterValue.id} >
                             <Row gutter={10}>
                                 <Col span={16}>
-                                    <Input defaultValue={parameterValue} style={{ marginBottom: 10 }} />
-                                </Col>
+                                    <Input defaultValue={parameterValue.value} style={{ marginBottom: 10 }} disabled /></Col>
                                 <Col span={8}>
                                     <Tooltip title="Copy">
-                                        <Button
-                                            type="primary"
-                                            style={{ boxShadow: "none" }}
-                                            icon={<Copy />}
-                                            onClick={() => handleCopy(parameterValue)}
-                                        />
+                                        <Button type="primary" style={{ boxShadow: "none" }} icon={<Copy />} onClick={() => handleCopy(parameterValue.value)} />
                                     </Tooltip>
                                 </Col>
                             </Row>
-                        ) : (
-                            <Row gutter={10}>
-                                <Col span={16}>
-                                    <Input defaultValue={parameterValue} style={{ marginBottom: 10 }} />
-                                </Col>
-                                <Col span={8}>
-                                    {parameterValue ? (
-                                        <Tooltip title="Copy">
-                                            <Button
-                                                type="primary"
-                                                style={{ boxShadow: "none" }}
-                                                icon={<Copy />}
-                                                onClick={() => handleCopy(parameterValue)}
-                                            />
-                                        </Tooltip>
-                                    ) : (
-                                        <Button onClick={generateApiKey} style={{ marginTop: 20, backgroundColor: "#1890ff", color: "#fff" }}>
-                                                    {t("ganerateKey")}
-                                        </Button>
-                                    )}
-                                </Col>
-                            </Row>
-                        )}
-                    </div>
-                );
-            })}
+                        </div>
+                    ))}
+                    {serviceParameterValuesList?.result?.find(paramVal => paramVal.parameter != undefined && paramVal.parameter.type === "token") == null ?
+                        < Row gutter={24}>
+                            <Col md={16} xs={24}>
+                                <Input defaultValue={""} placeholder={t('placeholderGenerate')} disabled /></Col>
+                            <Col md={8} xs={24}>
+                                <Button
+                                    onClick={generateApiKey}
+                                    style={{ backgroundColor: "#1890ff", color: "#fff" }}>
+                                    {t('ganerateKey')}
+                                </Button>
+                            </Col>
+                        </Row > :
+                        < Row>
+                            <SyntaxHighlighter language="bash" style={dracula}>
+                                {currentCartLine?.service.curl_command}
+                            </SyntaxHighlighter>
+                        </Row>
+                    }
+                </>
+            }
         </>
     );
 }
