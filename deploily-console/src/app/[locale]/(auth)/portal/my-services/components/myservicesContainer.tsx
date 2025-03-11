0fe119570630f9b1f1d18ed73955cf8b0a80b70f
bentayeb dounia <dounia.bentayeb@transformatek.dz>
2025-03-11 13:24:50 +0000

    get my services and remove fetchCartLines

--- deploily-console/src/app/[locale]/(auth)/portal/my-services/components/myservicesContainer.tsx
+++ deploily-console/src/app/[locale]/(auth)/portal/my-services/components/myservicesContainer.tsx
@@ -1,23 +1,24 @@
 import { useEffect } from "react";
 import { Row, Col } from "antd";
-import { useCartLine } from "@/lib/features/cartLine/cartLineSelectors";
 import { useAppDispatch } from "@/lib/hook";
 import MyServiceCard from "./myServicesCard";
-import { fetchCartLines } from "@/lib/features/cartLine/cartLineThunks";
+import { fetchMyServices } from "@/lib/features/myService/myServiceThunks";
+import { useMyService } from "@/lib/features/myService/myServiceSelectors";
+import { MyServiceInterface } from "@/lib/features/myService/myServiceInterface";
 
 export default function MyServiceContentPage() {
-  const { isLoading, cartLineResponse } = useCartLine();
   const dispatch = useAppDispatch();
+  const {myServiceLoading, myServiceResponse} = useMyService()
   
   useEffect(() => {
-    dispatch(fetchCartLines());
+    dispatch(fetchMyServices());
   }, []);
 
   return (
     <>
-      {!isLoading && cartLineResponse !== undefined && (
+      {!myServiceLoading && myServiceResponse !== undefined && (
         <Row gutter={[24, 24]} justify="start" style={{ margin: 0 }}>
-          {cartLineResponse?.result?.map((row) => (
+          {myServiceResponse?.result?.map((row: MyServiceInterface) => (
             <Col
               key={row.id}
               xs={24}
