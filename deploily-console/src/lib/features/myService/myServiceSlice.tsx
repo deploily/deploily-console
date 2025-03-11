bfa8628abe8489f1bce657eeca94b3f301660ae2
bentayeb dounia <dounia.bentayeb@transformatek.dz>
2025-03-11 13:25:57 +0000

    add post and get by id of my service

--- deploily-console/src/lib/features/myService/myServiceSlice.tsx
+++ deploily-console/src/lib/features/myService/myServiceSlice.tsx
@@ -1,18 +1,31 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { MyServiceResponse } from "./myServiceInterface";
-import { fetchMyServices } from "./myServiceThunks";
+import { MyServiceInterface, MyServiceResponse } from "./myServiceInterface";
+import { fetchMyServices, fetchMyServiceById, generateTokenThunk } from "./myServiceThunks";
 
 interface MyServiceState {
-  MyServiceResponse?: MyServiceResponse;
+  myServiceResponse?: MyServiceResponse;
   myServiceLoadingError?: any;
   myServiceLoading: boolean;
-
+  currentMyService?: MyServiceInterface;
+  currentMyServiceLoadingError?: any;
+  currentMyServiceLoading: boolean;
+  cartLine_id?: number;
+  generateTokenLoading?: boolean;
+  generatedToken?: string;
+  generateTokenFailed?: boolean;
 }
 
 const initialState: MyServiceState = {
-  MyServiceResponse: undefined,
+  myServiceResponse: undefined,
   myServiceLoadingError: undefined,
   myServiceLoading: false,
+  currentMyService: undefined,
+  currentMyServiceLoadingError: undefined,
+  currentMyServiceLoading: false,
+  cartLine_id: undefined,
+  generateTokenLoading: undefined,
+  generatedToken: undefined,
+  generateTokenFailed: undefined,
 };
 const MyServiceSlice = createSlice({
   name: "myService",
@@ -30,13 +43,38 @@ const MyServiceSlice = createSlice({
           Object.assign({}, { id: id }, action.payload.result[index]),
         );
         const payload = Object.assign({}, action.payload, { result: result });
-        state.MyServiceResponse = payload;
+        state.myServiceResponse = payload;
       })
       .addCase(fetchMyServices.rejected, (state, { payload }) => {
         state.myServiceLoading = false;
         state.myServiceLoadingError = payload;
       })
-
+      .addCase(fetchMyServiceById.pending, (state) => {
+        state.currentMyServiceLoading = true;
+        state.currentMyServiceLoadingError = null;
+      })
+      .addCase(fetchMyServiceById.fulfilled, (state, action) => {
+        state.currentMyServiceLoading = false;
+        state.currentMyServiceLoadingError = null;
+        state.currentMyService = { ...action.payload.result, ...{ id: action.payload.id } };
+        state.cartLine_id = action.payload.id;
+      })
+      .addCase(fetchMyServiceById.rejected, (state, { payload }) => {
+        state.currentMyServiceLoading = false;
+        state.currentMyServiceLoadingError = payload;
+      })   //GENRATE API TOKEN 
+      .addCase(generateTokenThunk.pending, (state) => {
+        state.generateTokenLoading = true;
+        state.generatedToken = undefined;
+      })
+      .addCase(generateTokenThunk.fulfilled, (state, action) => {
+        state.generateTokenLoading = false;
+        state.generatedToken = action.payload;
+      })
+      .addCase(generateTokenThunk.rejected, (state) => {
+        state.generateTokenLoading = false;
+        state.generateTokenFailed = true;
+      });
   },
 });
 export default MyServiceSlice.reducer;
--- deploily-console/src/lib/features/myService/myServiceThunks.tsx
+++ deploily-console/src/lib/features/myService/myServiceThunks.tsx
@@ -35,4 +35,85 @@ export const fetchMyServices = createAsyncThunk(
     }
   },
 );
+export const fetchMyServiceById = createAsyncThunk(
+  "myServices/getMyService",
+  async (myService_id: string, thunkConfig) => {
+      try {
+          const session = await getSession();
+          if (!session) {
+              return thunkConfig.rejectWithValue("session expired");
+          }
+          const token = session.accessToken;
+
+          const response = await axios.get(`${deploilyApiUrls.My_SERVICE_URL}${myService_id}`, {
+              headers: {
+                  Accept: "application/json",
+                  Authorization: `Bearer ${token}`,
+              },
+          });
+          if (response.status === 200) {
+              return response.data;
+          } else {
+              return thunkConfig.rejectWithValue("Failed to fetch my service");
+          }
+      } catch (error: any) {
+          return thunkConfig.rejectWithValue(error.response.data.message);
+      }
+  },
+);
+
+export const postMyService = createAsyncThunk(
+  "myServices/postMyService",
+  async (data: any, thunkConfig) => {
+    console.log("postMyService=== ", data);
+    
+    try {
+      const session = await getSession();
+      if (!session) {
+        return thunkConfig.rejectWithValue("session expired");
+      }
+      const token = session.accessToken;
+
+      const response = await axios.post(`${deploilyApiUrls.My_SERVICE_URL}`, data, {
+        headers: {
+          Accept: "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (response.status === 200) {
+        return response.data;
+      } else {
+        return thunkConfig.rejectWithValue("Failed to create new my service");
+      }
+    } catch (error: any) {
+      return thunkConfig.rejectWithValue(error.message);
+    }
+  },
+);
+
+export const generateTokenThunk = createAsyncThunk(
+  "myServices/generateToken",
+  async (myService_id: string, thunkConfig) => {
+      try {
+          const session = await getSession();
+          if (!session) {
+              return thunkConfig.rejectWithValue("session expired");
+          }
+          const token = session.accessToken;
+          const response = await axios.post(`${deploilyApiUrls.My_SERVICE_URL}${myService_id}/consumer`,{}, {
+              headers: {
+                  Accept: "application/json",
+                  Authorization: `Bearer ${token}`,
+              },
+          });
+          if (response.status === 200) {
+              return response.data['auth-key'];
+          } else {
+              return thunkConfig.rejectWithValue("Failed to generate token");
+          }
+      } catch (error: any) {
+          return thunkConfig.rejectWithValue(error.response.data.message);
+      }
+  },
+);
 
