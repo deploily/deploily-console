eda6743690545b06676b805a9702066f55de1485
bentayeb dounia <dounia.bentayeb@transformatek.dz>
2025-03-12 13:41:29 +0000

    add color

--- deploily-console/src/app/[locale]/(auth)/portal/service/[id]/components/getItems.tsx
+++ deploily-console/src/app/[locale]/(auth)/portal/service/[id]/components/getItems.tsx
@@ -3,17 +3,18 @@
 import { CollapseProps, Typography } from "antd";
 import { ApiServiceInterface } from "@/lib/features/apiService/apiServiceInterface";
 import Link from "next/link";
+import { theme } from "@/styles/theme";
 
 export const getItems = (row: ApiServiceInterface, t: any ): CollapseProps["items"] => [
   {
     key: "1",
     label: (
-      <Typography.Title level={3} style={{ fontWeight: 400, color: "#D85912" }}>
+      <Typography.Title level={3} style={{ fontWeight: 700, fontSize:24, color: theme.token.orange_7 }}>
         {t("description")}
       </Typography.Title>
     ),
     children: (
-      <Typography.Paragraph style={{ fontWeight: 400 }}>
+      <Typography.Paragraph style={{ fontWeight: 600 }}>
         {row.description}
       </Typography.Paragraph>
     ),
@@ -21,12 +22,12 @@ export const getItems = (row: ApiServiceInterface, t: any ): CollapseProps["item
   {
     key: "2",
     label: (
-      <Typography.Title level={3} style={{ fontWeight: 400, color: "#D85912" }}>
+      <Typography.Title level={3} style={{ fontWeight: 700, fontSize:24, color: theme.token.orange_7 }}>
         {t("featuresSpecifications")}
       </Typography.Title>
     ),
     children: (
-      <Typography.Paragraph style={{ fontWeight: 400 }}>
+      <Typography.Paragraph style={{ fontWeight: 600 }}>
         {row.specifications}
       </Typography.Paragraph>
     ),
@@ -37,7 +38,7 @@ export const getItems = (row: ApiServiceInterface, t: any ): CollapseProps["item
         key: "3",
         label: (
           <Link href={row.documentation_url} target="_blank" rel="noopener noreferrer">
-            <Typography.Title level={3} style={{ fontWeight: 400, color: "#D85912" }}>
+            <Typography.Title level={3} style={{ fontWeight: 700, fontSize:24, color: theme.token.orange_7 }}>
               {t("documentation")}
             </Typography.Title>
           </Link>
