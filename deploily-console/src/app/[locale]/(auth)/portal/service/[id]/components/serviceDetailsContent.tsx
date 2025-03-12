b7fbd575fffae6cc359aaf61d922a11e1171d803
bentayeb dounia <dounia.bentayeb@transformatek.dz>
2025-03-12 13:42:12 +0000

    get service plan & fixes design

--- deploily-console/src/app/[locale]/(auth)/portal/service/[id]/components/serviceDetailsContent.tsx
+++ deploily-console/src/app/[locale]/(auth)/portal/service/[id]/components/serviceDetailsContent.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Col, Row, Image, Typography, theme, Collapse, Card, Button, Space, Skeleton, Badge } from "antd";
+import { Col, Row, Image, Typography, Collapse, Button, Space, Skeleton, Badge } from "antd";
 import { ArrowLeft, CaretDown, CaretUp, Star } from "@phosphor-icons/react";
 import { useI18n } from "../../../../../../../../locales/client";
 import { getItems } from "./getItems";
@@ -12,18 +12,23 @@ import Paragraph from "antd/es/typography/Paragraph";
 import { IMAGES_URL } from "@/deploilyWebsiteUrls";
 import { postFavoriteService } from "@/lib/features/favorites/favoriteServiceThunks";
 import { useFavoriteServices } from "@/lib/features/favorites/favoriteServiceSelectors";
-import { postMyService } from "@/lib/features/myService/myServiceThunks";
+import { theme } from "@/styles/theme";
+import { fetchServicePlan } from "@/lib/features/servicePlan/servicePlanThanks";
+import { useServicePlan } from "@/lib/features/servicePlan/servicePlanSelector";
+import { ServicePlan } from "@/lib/features/servicePlan/servicePlanInterface";
+import ServicePlanCard from "./servicePlanCard";
 
 export default function ServiceDetailsContentPage({ serviceId }: { serviceId: string }) {
-  const { useToken } = theme;
-  const { token } = useToken();
+
   const t = useI18n();
   const { currentService, serviceLoading } = useAllServices();
   const { favoriteServiceAdded, favoriteServiceDeleted } = useFavoriteServices()
   const dispatch = useAppDispatch();
   const router = useRouter();
+  const { servicePlanResponse, servicePlanLoading } = useServicePlan()
   useEffect(() => {
     dispatch(getApiServiceById(serviceId));
+    dispatch(fetchServicePlan(serviceId))
   }, [favoriteServiceAdded, favoriteServiceDeleted]);
 
   const imageUrl = (image_service: string) => {
@@ -45,7 +50,7 @@ export default function ServiceDetailsContentPage({ serviceId }: { serviceId: st
         <Button style={{ border: "none", background: "#030303", boxShadow: "none" }}
           icon={<ArrowLeft color="#D85912" size={35} />} onClick={() => router.back()} />
       </div>
-      <Space direction="vertical" size="large" style={{ paddingInline: 40, marginTop: 10, width: "100%" }}>
+      <Space direction="vertical" size="large" style={{ paddingInline: 40, marginBlock: 10, width: "100%" }}>
         {(serviceLoading || currentService === undefined) ?
           <>
             <Skeleton.Image active />
@@ -63,7 +68,7 @@ export default function ServiceDetailsContentPage({ serviceId }: { serviceId: st
                     handleFavoriteService(currentService.id)
                   }
                 />}
-                offset={[-20, 20]}>
+                  offset={[-20, 20]}>
                   <Image
                     alt="Logo"
                     src={imageUrl(currentService?.image_service)}
@@ -76,15 +81,12 @@ export default function ServiceDetailsContentPage({ serviceId }: { serviceId: st
               </Col>
 
               <Col md={8} xs={24} style={{
-                color: "#DD8859",
-                fontWeight: "bold",
-                fontSize: "18px",
                 display: "flex",
-                justifyContent: "start",
+                justifyContent: "end",
                 alignSelf: "start"
               }}>
-                <Typography.Title level={2} style={{ color: token.colorPrimaryTextHover }}>
-                  {currentService.unit_price}
+                <Typography.Title level={2} style={{ color: theme.token.orange_6 }}>
+                  {currentService.unit_price} DZD
                 </Typography.Title>
               </Col>
             </Row>
@@ -95,88 +97,53 @@ export default function ServiceDetailsContentPage({ serviceId }: { serviceId: st
 
             </Row>
             <Row gutter={16} style={{ marginTop: 10 }} >
-              <Col span={16} >
-                <div>
-                  <Paragraph style={{ fontSize: 18 }} >
+              <div>
+                <Paragraph style={{ fontSize: 18 }} >
 
-                    {currentService.description}
-                  </Paragraph>
-                </div>
-              </Col>
+                  {currentService.short_description}
+                </Paragraph>
+              </div>
 
             </Row>
             <Row gutter={16} key={currentService.id}  >
-              <Col md={16} xs={24} style={{ textAlign: "start", width: "100%", padding: 0, marginTop: 10 }}>
-                <Collapse
-                  bordered={false}
-                  defaultActiveKey={["1"]}
-                  expandIcon={({ isActive }) => (isActive ? <CaretUp /> : <CaretDown />)}
-                  expandIconPosition="end"
-                  style={{ background: token.colorBgContainer, width: "100%" }}
-                  items={getItems(currentService, t)}
-                />
+              <Collapse
+                bordered={false}
+                defaultActiveKey={["1"]}
+                expandIcon={({ isActive }) => (isActive ? <CaretUp /> : <CaretDown />)}
+                expandIconPosition="end"
+                style={{ background: theme.token.darkGray_1, border: "1px solid", borderColor: theme.token.gray_1, width: "100%" }}
+                items={getItems(currentService, t)}
+              />
+
+            </Row>
+            <Row gutter={20}>
+              <Col span={24}>
+                <Typography.Title level={2} style={{ color: theme.token.blue_1, fontSize: 24, }}>
+                  {t('SelectServicePlan')}
+                </Typography.Title>
               </Col>
+              {servicePlanLoading || servicePlanResponse?.result === undefined ?
 
-              <Col md={8} xs={24}
-                style={{
-                  paddingLeft: 10,
-                  marginTop: 10,
-                  textAlign: "center",
-                  display: "flex",
-                  justifyContent: "start",
-                }}
-              >
-                <Card style={{ width: "90%", maxWidth: 300, height: 250 }}>
-                  <Typography style={{ color: "#7D7D7D", paddingBottom: 10 }}>{t("offeredBy")}</Typography>
-                  <Col style={{ paddingBottom: 10, textAlign: "center" }}>
-                    <Image alt="Logo" src="/images/logo_transformatek.png" width={70} height={70} />
-                  </Col>
-                  <Typography style={{ paddingBottom: 10 }}>{t("oneMonthFree")}</Typography>
-
-                  <Button
-                    style={{
-                      color: "#fff",
-                      backgroundColor: "#D85912",
-                      border: "none",
-                      padding: "4px",
-                    }}
-                    onClick={() => {
-                      dispatch(postMyService({
-                        "name": currentService.name,
-                        "amount": currentService.unit_price,
-                        "created_on": Date.now(),
-                        "duration_month": 1,
-                        "start_date": Date.now(),
-                        "description": currentService.description,
-                        "documentation_url": currentService.documentation_url,
-                        "unit_price": currentService.unit_price,
-                        "service_url": currentService.service_url,
-                        "image_service": currentService.image_service,
-                        "short_description": currentService.short_description,
-                        "specifications": currentService.specifications,
-                        "curl_command": currentService.curl_command,
-                      }));
-                      router.push("/portal/my-services")
-                    }}
-                  >
-                    <span
-                      style={{
-                        color: "rgba(220, 233, 245, 0.88)",
-                        fontFamily: "Inter, sans-serif",
-                        fontSize: "16px",
-                        fontWeight: 600,
-                      }}
+                <Skeleton.Node active style={{ width: 160, height: 200 }} /> :
+                <>
+                  {servicePlanResponse?.result?.map((row: ServicePlan) => (
+                    <Col
+                      key={row.id}
+                      xs={24}
+                      sm={12}
+                      md={10}
+                      lg={8}
+                      xl={8}
+                      style={{ display: "flex", justifyContent: "center" }}
                     >
-                      {t("TryForFree")}
-                    </span>
-                  </Button>
-                </Card>
-              </Col>
+                      <ServicePlanCard key={row.id} servicePlan={row} />
+
+                    </Col>
+                  ))}
+                </>
+              }
             </Row>
           </>
-
-
-
         }
 
       </Space>
