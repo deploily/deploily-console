b94c8cfa5f2b1e65bfdeca91a3b5cdff2e8b6a1c
bentayeb dounia <dounia.bentayeb@transformatek.dz>
2025-03-16 12:13:38 +0000

    modify interface

--- deploily-console/src/lib/features/myService/myServiceInterface.tsx
+++ deploily-console/src/lib/features/myService/myServiceInterface.tsx
@@ -11,9 +11,12 @@ export interface MyServiceResponse {
 export interface MyServiceInterface {
   id: number;
   name: string;
-  amount:string,
-  created_on: Date,
-  duration_month: number
-  start_date: Date, 
-  changed_on: Date
+  amount:string;
+  created_on: Date;
+  duration_month: number;
+  start_date: Date;
+  changed_on: Date;
+  description: string;
+  image_service: string;
+  
 }
