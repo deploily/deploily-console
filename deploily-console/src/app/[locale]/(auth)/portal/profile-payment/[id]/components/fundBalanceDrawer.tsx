dd413159461796fb3069c7184f4d6b7fc072469a
bentayeb dounia <dounia.bentayeb@transformatek.dz>
2025-03-26 13:28:12 +0000

    add card

--- deploily-console/src/app/[locale]/(auth)/portal/profile-payment/[id]/components/fundBalanceDrawer.tsx
+++ deploily-console/src/app/[locale]/(auth)/portal/profile-payment/[id]/components/fundBalanceDrawer.tsx
@@ -1,19 +1,31 @@
 import { theme } from "@/styles/theme";
-import { Drawer, Flex } from "antd";
+import { Button, Card, Checkbox, Drawer, Flex, Radio, RadioChangeEvent, Image, Typography } from "antd";
 import { useScopedI18n } from "../../../../../../../../locales/client";
-import { BodyMedium, InterRegular16 } from "@/styles/components/typographyStyle";
+import { InterRegular16 } from "@/styles/components/typographyStyle";
 
-import type { CheckboxGroupProps } from 'antd/es/checkbox';
-import { CustomRadioGroup } from "@/styles/components/radioStyle";
-import { CustomDrawerCard } from "@/styles/components/cardStyle";
+import type { CheckboxChangeEvent } from 'antd/es/checkbox';
+import { useState } from "react";
 
-const options: CheckboxGroupProps<string>['options'] = [
-    { label: 'Card', value: 'card' },
-    { label: 'Bank transfer', value: 'bankTransfer' },
-];
+
 
 export default function FundBalanceDrawer({ openDrawer, onClose }: { openDrawer: any, onClose: any }) {
     const t = useScopedI18n("profilePayment");
+    const [value, setValue] = useState(false);
+    const [values, setValues] = useState({
+        payment_method: "card",
+        balanceRechage: 0,
+    });
+
+    const onChange = (e: RadioChangeEvent) => {
+
+        setValues({ ...values, payment_method: e.target.value });
+
+    };
+
+    const onChangeCheckbox = (e: CheckboxChangeEvent) => {
+        setValue(e.target.value);
+
+    };
 
     return (
         <Drawer
@@ -24,20 +36,76 @@ export default function FundBalanceDrawer({ openDrawer, onClose }: { openDrawer:
             getContainer={false}
             width={600}
             styles={{
-                header: { backgroundColor: theme.token.darkGray, borderBottom: `1px solid ${theme.token.gray200}` },
+                header: { fontSize: 20, backgroundColor: theme.token.darkGray, borderBottom: `1px solid ${theme.token.gray200}` },
                 body: { padding: 20, backgroundColor: theme.token.darkGray },
             }}
         >
-            <BodyMedium>{t('choosePaymentMethod')}</BodyMedium>
-            <Flex style={{ marginTop: "20px", paddingBlock: 10, backgroundColor: theme.token.colorBlack, borderRadius: 5 }} vertical gap="middle">
-                <CustomRadioGroup block options={options} defaultValue="card" />
-            </Flex>
-            <CustomDrawerCard style={{ borderRadius: 0, marginBottom:0 }} >
-                <BodyMedium>CIB/ E-Dahabia</BodyMedium>
-            </CustomDrawerCard>
-            <CustomDrawerCard style={{ borderRadius: 0, borderTop:"none", marginTop:0 }} >
-                <InterRegular16> {t('balanceRecharge')} : </InterRegular16>
-            </CustomDrawerCard>
+           
+            <>
+                <Typography.Title level={4} style={{ paddingTop: 20, paddingBottom: 20 }}>{t('choosePaymentMethod')}</Typography.Title>
+                <Flex vertical gap="start" style={{ padding: 10, backgroundColor: theme.token.colorBgBase, borderRadius: 5 }}>
+                    <Radio.Group block defaultValue="card"
+                        onChange={onChange}
+                        value={values.payment_method}>
+                        <Radio value="card"  >Card</Radio>
+                        <Radio value="bank_transfer">Bank transfer</Radio>
+                    </Radio.Group>
+
+                </Flex>
+                <Card
+                    title="CIB/ E-Dahabia" style={{
+                        marginTop: 20,
+                        display: "flex",
+                        flexDirection: "column",
+                        // height: "100%",
+                        borderColor: theme.token.gray50,
+                        boxShadow: "none",
+                        textAlign: "center", borderRadius: 0
+                    }}
+                    styles={{ header: { borderColor: theme.token.gray50, textAlign: "start" } }}
+
+                >
+                    <div style={{ display: "flex", flexDirection: "column", alignItems: "start", gap: 10 }}>
+                        <InterRegular16> {t('balanceRecharge')} : </InterRegular16>
+                    </div>
+                   
+                    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: 10 }}>
+                       
+
+                        <Button
+                            onClick={() => console.log(values)}
+                            style={{
+                                color: "#fff",
+                                backgroundColor: "#D85912",
+                                border: "none",
+                                padding: "4px 8px",
+                                display: "flex",
+                                alignItems: "center",
+                                gap: "6px",
+                            }} > captcha</Button>
+                        <Checkbox style={{ padding: 15 }} onChange={onChangeCheckbox} value={value}>I accept the general conditions of use</Checkbox>
+
+                        <Button
+                            style={{
+                                color: "#fff",
+                                backgroundColor: theme.token.blue300,
+                                border: "none",
+                                padding: "25px 10px",
+                                display: "flex",
+                                alignItems: "center",
+                                gap: "10px",
+                            }}
+                            icon={<Image src="/images/paymentIcon.png" alt="PAY" style={{ width: 60, height: 35 }} />}
+                        // onClick={handleSubscribe}
+                        >
+
+                            <span style={{ fontFamily: "Inter, sans-serif", fontSize: "16px", fontWeight: 600 }}>
+                                PAY
+                            </span>
+                        </Button>
+                    </div>
+                </Card>
+            </>
         </Drawer>
     )
 }
