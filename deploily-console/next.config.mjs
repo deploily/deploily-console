31dadf2f19b4d5ecb293fbf3907fac6d768e9104
Ali BENAHMED DAHO <ali.benahmeddaho@transformatek.dz>
2025-04-21 03:17:50 +0100

    Move BASE_URL to tsx file

--- deploily-console/next.config.mjs
+++ deploily-console/next.config.mjs
@@ -1,16 +1,8 @@
 
-// const BASE_URL = "http://192.168.1.21:5000"; // for DEV  
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;  // for PROD
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     transpilePackages: ['next-international', 'international-types'],
     output: "standalone",
-    env: {
-        BASE_URL: `${BASE_URL}`,
-        API_BASE_URL: `${BASE_URL}/api/v1`,
-        WEB_SITE_URL: "https://console.deploily.cloud",
-    },
     images: {
         remotePatterns: [
             {
--- deploily-console/src/deploilyWebsiteUrls.ts
+++ deploily-console/src/deploilyWebsiteUrls.ts
@@ -1,24 +1,28 @@
-export const BASE_URL = `${process.env.API_BASE_URL}`;
-export const IMAGES_URL = `${process.env.BASE_URL}/static/uploads/`;
+
+// const BASE_URL = "http://192.168.1.21:5000"; // for DEV  
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;  // for PROD
+
+const API_BASE_URL = `${BASE_URL}/api/v1`;
+export const IMAGES_URL = `${BASE_URL}/static/uploads/`;
 export const DOC_URL = "https://docs.deploily.cloud";
 
 export const deploilyApiUrls = {
-  SERVICE_URL: `${process.env.API_BASE_URL}/service/`,
-  SERVICE_PLAN_URL: `${process.env.API_BASE_URL}/service-plan`,
-  SUBSCRIBE_URL: `${process.env.API_BASE_URL}/subscription/`,
-  MY_FAVORITE_URL: `${process.env.API_BASE_URL}/my-favorites/`,
-  POST_FAVORITE_SERVICE_URL: `${process.env.API_BASE_URL}/my-favorites-service/`,
-  SUPPORT_TICKET_URL: `${process.env.API_BASE_URL}/support-ticket/`,
-  SUPPORT_TICKET_RESPONSES_URL: `${process.env.API_BASE_URL}/support-ticket-response/`,
-  LOGIN_URL: `${process.env.API_BASE_URL}/security/login/`,
-  SERVICE_PARAMETER_URL: ` ${process.env.API_BASE_URL}/parameter/`,
-  SERVICE_PARAMETER_VALUES_URL: ` ${process.env.API_BASE_URL}/parameter-value/`,
-  SERVICE_CONSUMER: ` ${process.env.API_BASE_URL}/my-service/`,
-  PAYMENT_PROFILE_URL: ` ${process.env.API_BASE_URL}/payment-profile/`,
-  PAYMENT: ` ${process.env.API_BASE_URL}/payments/`,
+  SERVICE_URL: `${API_BASE_URL}/service/`,
+  SERVICE_PLAN_URL: `${API_BASE_URL}/service-plan`,
+  SUBSCRIBE_URL: `${API_BASE_URL}/subscription/`,
+  MY_FAVORITE_URL: `${API_BASE_URL}/my-favorites/`,
+  POST_FAVORITE_SERVICE_URL: `${API_BASE_URL}/my-favorites-service/`,
+  SUPPORT_TICKET_URL: `${API_BASE_URL}/support-ticket/`,
+  SUPPORT_TICKET_RESPONSES_URL: `${API_BASE_URL}/support-ticket-response/`,
+  LOGIN_URL: `${API_BASE_URL}/security/login/`,
+  SERVICE_PARAMETER_URL: ` ${API_BASE_URL}/parameter/`,
+  SERVICE_PARAMETER_VALUES_URL: ` ${API_BASE_URL}/parameter-value/`,
+  SERVICE_CONSUMER: ` ${API_BASE_URL}/my-service/`,
+  PAYMENT_PROFILE_URL: ` ${API_BASE_URL}/payment-profile/`,
+  PAYMENT: ` ${API_BASE_URL}/payments/`,
   PAYMENT_RECEIPT: `/upload-receipt`,
-  SERVICE_SUBSCRIPTION: `${process.env.API_BASE_URL}/service-subscription/subscribe`,
-  CHECK_PROMO_CODE: `${process.env.API_BASE_URL}/promo-code/`,
-  EPAYMENT_STATUS: `${process.env.API_BASE_URL}/service-subscription/payment-status`,
-  CONTACT_US: `${process.env.API_BASE_URL}/contact-us/`,
+  SERVICE_SUBSCRIPTION: `${API_BASE_URL}/service-subscription/subscribe`,
+  CHECK_PROMO_CODE: `${API_BASE_URL}/promo-code/`,
+  EPAYMENT_STATUS: `${API_BASE_URL}/service-subscription/payment-status`,
+  CONTACT_US: `${API_BASE_URL}/contact-us/`,
 };
