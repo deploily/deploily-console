b996022bf3ee0d30ea6792cd534283fe535056fe
ziadi kheira <kheira.ziadi@transformatek.dz>
2025-06-12 15:45:57 +0000

    Refactor application service thunks / get apps from api.

--- deploily-console/src/lib/features/application/applicationServiceThunks.tsx
+++ deploily-console/src/lib/features/application/applicationServiceThunks.tsx
@@ -1,17 +1,62 @@
+import axiosInstance from "@/app/api/axios-instance";
+import { deploilyApiUrls } from "@/deploilyWebsiteUrls";
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { data } from "./data";
+import { getSession } from "next-auth/react";
 
 export const fetchApplicationServices = createAsyncThunk(
     "applicationService/getapplicationService",
     async (_, thunkConfig) => {
+
         try {
-            const response = data;
+            const session = await getSession();
 
-            if (response.status == 200) {
+            if (!session) {
+                return thunkConfig.rejectWithValue("session expired");
+            }
 
+            const token = session.accessToken;
+
+            const response = await axiosInstance.get(`${deploilyApiUrls.APPP_SERVICES_URL}`, {
+                headers: {
+                    Accept: "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+
+            if (response.status === 200) {
                 return response.data;
             } else {
-                return thunkConfig.rejectWithValue("error");
+                return thunkConfig.rejectWithValue("Failed to fetch services");
+            }
+        } catch (error: any) {
+            return thunkConfig.rejectWithValue(error.message);
+        }
+    },
+);
+
+export const fetchApplicationServiceById = createAsyncThunk(
+    "applicationService/getApplicationServiceById",
+    async (app_id, thunkConfig) => {
+        try {
+            const session = await getSession();
+
+            if (!session) {
+                return thunkConfig.rejectWithValue("session expired");
+            }
+
+            const token = session.accessToken;
+
+            const response = await axiosInstance.get(`${deploilyApiUrls.APPP_SERVICES_URL}${app_id}`, {
+                headers: {
+                    Accept: "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+
+            if (response.status === 200) {
+                return response.data;
+            } else {
+                return thunkConfig.rejectWithValue("Failed to fetch service by id");
             }
         } catch (error: any) {
             return thunkConfig.rejectWithValue(error.message);
