9581ec56717503d3fb1bac32ee6f68838efac896
bentayeb dounia <dounia.bentayeb@transformatek.dz>
2025-06-17 14:38:23 +0000

    update myApplicationInterface and add MyApplicationByIdState interface

--- deploily-console/src/lib/features/my-applications/myApplicationInterface.tsx
+++ deploily-console/src/lib/features/my-applications/myApplicationInterface.tsx
@@ -1,5 +1,11 @@
+
 export interface MyApplicationState {
   MyApplicationList?: myApplicationResponse;
+  isLoading: boolean;
+  loadingError?: any;
+}
+export interface MyApplicationByIdState {
+  myApplicationsById?: myApplicationInterface;
   isLoading: boolean;
   loadingError?: any;
 }
@@ -17,20 +23,20 @@ export interface myApplicationResponse {
 export interface myApplicationInterface {
   id: number;
   name: string;
-  description: string;
+  api_key: string;
   application_status: string;
   price: number;
-  duration_month: string;
-  is_expired: string;
+  duration_month: number;
+  is_expired: boolean;
   service_plan_id: number;
   start_date: Date;
   total_amount: number;
-  api_key: string;
-  service_details: string;
+  service_details: ServiceDetails;
   service_plan: Service_plan;
   is_in_favorite: string;
   image_service: string;
-  short_description: string
+  short_description: string;
+  status: string;
 
 }
 
@@ -41,5 +47,22 @@ interface Service_plan {
   subscription_category: string;
 
 }
+ interface ServiceDetails {
+    id: number;
+    description: string;
+    documentation_url:string;
+    image_service: string;
+    monitoring: string;
+    name: string;
+    service_slug: string;
+    short_description: string;
+    specifications: string;
+    ssh_access:string;
+    type: string;
+    unit_price:  number;
+  
+ }
+
+
 
 
