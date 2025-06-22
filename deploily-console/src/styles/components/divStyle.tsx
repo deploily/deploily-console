1edffa9cae94899d2fc136400c278744c22dc7b4
bentayeb dounia <dounia.bentayeb@transformatek.dz>
2025-06-22 15:06:02 +0000

    add div style

---
+++ deploily-console/src/styles/components/divStyle.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const DivCard = styled.div` &&
+ {  
+    border: 1px solid rgba(68, 66, 66, 1);
+    border-radius: 8px;
+    background-color: #212020;
+    borderColor: #D0CECE;
+    width: "100%";
+    padding:10px;
+    margin-top: 20px;
+
+}
+`;
