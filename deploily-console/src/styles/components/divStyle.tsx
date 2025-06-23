a050a5f27a7b353b04a440eee722217f9b180605
bentayeb dounia <dounia.bentayeb@transformatek.dz>
2025-06-23 11:18:18 +0000

    fixes

--- deploily-console/src/styles/components/divStyle.tsx
+++ deploily-console/src/styles/components/divStyle.tsx
@@ -5,10 +5,9 @@ export const DivCard = styled.div` &&
     border: 1px solid rgba(68, 66, 66, 1);
     border-radius: 8px;
     background-color: #212020;
-    borderColor: #D0CECE;
     width: "100%";
-    padding:10px;
-    margin-top: 20px;
+    padding:20px;
+    margin-bottom: 20px;
 
 }
 `;
