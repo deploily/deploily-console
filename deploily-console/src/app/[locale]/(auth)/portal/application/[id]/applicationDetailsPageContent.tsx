0574f3fa3670260e3c4f62289286816061097b68
Sedjai Bouchra <bouchra.sedjai@transformatek.dz>
2025-06-25 11:01:01 +0000

    Refactor ApplicationDetailsPageContent for improved layout and responsiveness; add PaymentAppBar for mobile view

--- deploily-console/src/app/[locale]/(auth)/portal/application/[id]/applicationDetailsPageContent.tsx
+++ deploily-console/src/app/[locale]/(auth)/portal/application/[id]/applicationDetailsPageContent.tsx
@@ -1,26 +1,29 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import { Card, Col, Row, Select, Skeleton, Space, Grid } from "antd";
+import { useAppDispatch } from "@/lib/hook";
 import { useApplicationServiceById, useNewApplicationSubscription } from "@/lib/features/application/applicationServiceSelectors";
 import { fetchApplicationServiceById } from "@/lib/features/application/applicationServiceThunks";
 import { fetchServicePlans } from "@/lib/features/service-plans/servicePlanThanks";
-import { useAppDispatch } from "@/lib/hook";
+import { updateNewAppSubscriptionState } from "@/lib/features/application/applicationServiceSlice";
 import ImageFetcher from "@/lib/utils/imageFetcher";
-import { Card, Col, Row, Select, Skeleton, Space } from "antd";
-import { PaymentSideBar } from "deploily-ui-components";
-import { useEffect, useState } from "react";
-import { useScopedI18n } from "../../../../../../../locales/client";
 import ApplicationDetailsCollapseContainer from "./containers/applicationDetailsCollapseContainer";
 import ApplicationPlansContainer from "./containers/applicationPlansContainer";
 import ApplicationDescriptionContainer from "./containers/descriptionContainer";
 import SelectVpsPlanTable from "./containers/selectVpsPlanTable";
-import { theme } from "@/styles/theme";
-import { options } from "../../api-services/utils/apiServicesConst";
-import { updateNewAppSubscriptionState } from "@/lib/features/application/applicationServiceSlice";
+import { PaymentSideBar } from "deploily-ui-components";
+import { PaymentAppBar } from "deploily-ui-components/components/applications/paymentSideBar";
 import PaymentDrawer from "./containers/payment-components/paymentDrawer";
+import { useScopedI18n } from "../../../../../../../locales/client";
+import { options } from "../../api-services/utils/apiServicesConst";
+import { theme } from "@/styles/theme";
 
 export default function ApplicationDetailsPageContent({ applicationId }: { applicationId: any }) {
     const dispatch = useAppDispatch();
+    const screens = Grid.useBreakpoint();
     const [openDrawer, setOpenDrawer] = useState(false);
+    const [isScrolled, setIsScrolled] = useState(false);
     const onClose = () => setOpenDrawer(false);
 
     const { applicationServiceById, isLoading, loadingError } = useApplicationServiceById();
@@ -36,82 +39,136 @@ export default function ApplicationDetailsPageContent({ applicationId }: { appli
         dispatch(fetchServicePlans(applicationId));
     }, []);
 
+    useEffect(() => {
+        const handleScroll = () => setIsScrolled(window.scrollY > 100);
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+    if (isLoading) return <Skeleton active />;
+    if (loadingError) return <div>Error: {loadingError}</div>;
+    if (!applicationServiceById) return <div>No application found</div>;
+
     return (
         <>
-            {isLoading ? (
-                <div><Skeleton active /></div>
-            ) : loadingError ? (
-                <div>Error: {loadingError}</div>
-            ) : !applicationServiceById ? (
-                <div>No application found</div>
-            ) : (
+            <Space
+                direction="vertical"
+                size="large"
+                style={{
+                    paddingInline: 40,
+                    marginBlock: 10,
+                    width: "100%",
+                    marginBottom: 50,
+                    paddingTop: 20,
+                    justifyContent: "center",
+                }}
+            >
+                <Row gutter={[24, 24]} wrap style={{ justifyContent: "center",margin:'0px' }}>
+                    {/* Main Content */}
+                    <Col xs={24} md={24} lg={16} style={{ overflowY: 'auto',padding:'0px',margin:'0px' }}>
+                        <ApplicationDescriptionContainer
+                            title={applicationServiceById.name}
+                            description={applicationServiceById.short_description}
+                            logo={
+                                <div style={{ border: "1px solid #4E4E4E", borderRadius: "10px", padding: "1px" }}>
+                                    <ImageFetcher imagePath={applicationServiceById.image_service} width={220} height={220} />
+                                </div>
+                            }
+                        />
 
-                <Space direction="vertical" size="large" 
-                    style={{ paddingInline: 40, marginBlock: 10, width: "100%", marginBottom: 50, paddingTop: 20, justifyContent: "center" }}>
+                        <div style={{ padding: '8px 0' }}>
+                            <ApplicationPlansContainer />
+                        </div>
+                        {/* Payment App Bar - Only for Mobile */}
+                        {!screens.lg && (
+                            <div
 
-                                <Row gutter={[24, 24]} wrap style={{ justifyContent: "center" }}>
-                                    {/* Main Content */}
-                                    <Col xs={24} md={24} lg={16} style={{ overflowY: 'auto', paddingRight: '8px' }}>
-                                        <ApplicationDescriptionContainer
-                                            title={applicationServiceById.name}
-                                            description={applicationServiceById.short_description}
-                                            logo={
-                                                <div style={{ border: "1px solid #4E4E4E", borderRadius: "10px", padding: "1px" }}>
-                                                    <ImageFetcher imagePath={applicationServiceById.image_service} width={220} height={220} />
-                                                </div>
-                                            }
-                                        />
+                                style={{
+                                    position: isScrolled ? "fixed" : "relative",
+                                    bottom: isScrolled ? 0 : "auto",
+                                    left: 0,
+                                    right: 0,
+                                    width: "100%",
+                                    zIndex: 1000,
+                                    paddingBottom: '24px',
+                                    display: 'flex',
+                                    justifyContent: "center",
+                                    backgroundColor: isScrolled ? "#202227" : "transparent",
+                                    transition: "all 0.3s ease-in-out",
+                              }}
 
-                                        <div style={{ padding: '8px 0' }}>
-                                            <ApplicationPlansContainer />
-                                        </div>
+                            >
+                                <PaymentAppBar
+                                    price={totalAmount}
+                                    buttonText={tApplications('confirm')}
+                                    items={[
 
-                                        <Card styles={{ body: { padding: 0 } }}>
-                                            <SelectVpsPlanTable />
-                                        </Card>
+                                        {
+                                            label: tApplications('duration'),
+                                            value: (
+                                                <Select
+                                                    defaultValue={duration}
+                                                    style={{ width: 150, borderRadius: "10px" }}
+                                                    onChange={handleChangeDuration}
+                                                    dropdownStyle={{
+                                                        backgroundColor: theme.token.gray50,
+                                                        border: `2px solid ${theme.token.gray100}`,
+                                                    }}
+                                                    options={options}
+                                                />
+                                            ),
+                                        },
 
-                                        <div style={{ padding: '8px 0' }}>
-                                            <ApplicationDetailsCollapseContainer description={applicationServiceById.description} />
-                                        </div>
-                                    </Col>
+                                    ]}
+                                    onClick={() => setOpenDrawer(true)}
+                                />
+                            </div>
+                        )}
+                        <Card styles={{ body: { padding: 0 } }}>
+                            <SelectVpsPlanTable />
+                        </Card>
 
-                                    {/* Payment Sidebar */}
-                                    <Col xs={24} lg={8} md={0} style={{
-                                        position: 'sticky', top: 16, alignSelf: 'flex-start',
-                                        // justifyContent: 'center', display: 'flex'
-                                    }}>
-                                        <PaymentSideBar
-                                            price={totalAmount}
-                                            buttonText={tApplications('confirm')}
-                                            items={[
-                                                { label: tApplications('svc'), value: applicationServiceById.name },
-                                                { label: tApplications("plan"), value: app_service_plan?.plan.name || "" },
-                                                {
-                                                    label: tApplications('duration'),
-                                                    value: (
-                                                        <Select
-                                                            defaultValue={duration}
-                                                            style={{ width: 150, borderRadius: "10px" }}
-                                                            onChange={handleChangeDuration}
-                                                            dropdownStyle={{
-                                                                backgroundColor: theme.token.gray50,
-                                                                border: `2px solid ${theme.token.gray100}`,
-                                                            }}
-                                                            options={options}
-                                                        />
-                                                    ),
-                                                },
-                                                { label: tApplications('provider'), value: resource_service_plan?.provider_info?.name || "" },
-                                                { label: tApplications("vpsType"), value: resource_service_plan?.service?.name || "" },
-                                                { label: tApplications('resourcePlan'), value: resource_service_plan?.plan?.name || "" },
-                                            ]}
-                                            onClick={() => setOpenDrawer(true)}
-                                        />
-                                    </Col>
-                                </Row>
+                        <div style={{ padding: '8px 0' }}>
+                            <ApplicationDetailsCollapseContainer description={applicationServiceById.description} />
+                        </div>
+                    </Col>
 
-                </Space>
-            )}
+                    {/* Payment Sidebar - Only for Desktop */}
+                    {screens.lg && (
+                        <Col xs={24} lg={8} style={{ position: 'sticky', top: 16, alignSelf: 'flex-start' }}>
+                            <PaymentSideBar
+                                price={totalAmount}
+                                buttonText={tApplications('confirm')}
+                                items={[
+                                    { label: tApplications('svc'), value: applicationServiceById.name },
+                                    { label: tApplications("plan"), value: app_service_plan?.plan.name || "" },
+                                    {
+                                        label: tApplications('duration'),
+                                        value: (
+                                            <Select
+                                                defaultValue={duration}
+                                                style={{ width: 150, borderRadius: "10px" }}
+                                                onChange={handleChangeDuration}
+                                                dropdownStyle={{
+                                                    backgroundColor: theme.token.gray50,
+                                                    border: `2px solid ${theme.token.gray100}`,
+                                                }}
+                                                options={options}
+                                            />
+                                        ),
+                                    },
+                                    { label: tApplications('provider'), value: resource_service_plan?.provider_info?.name || "" },
+                                    { label: tApplications("vpsType"), value: resource_service_plan?.service?.name || "" },
+                                    { label: tApplications('resourcePlan'), value: resource_service_plan?.plan?.name || "" },
+                                ]}
+                                onClick={() => setOpenDrawer(true)}
+                            />
+                        </Col>
+                    )}
+                </Row>
+
+               
+            </Space>
 
             <PaymentDrawer openDrawer={openDrawer} onClose={onClose} />
         </>
