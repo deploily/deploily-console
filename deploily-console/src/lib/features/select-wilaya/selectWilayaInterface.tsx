447d6941f57b74094437a2271e0529fb56e4bae6
kawther boukra <kawther.boukra@transformatek.dz>
2025-07-14 15:02:54 +0000

    Add functionality to fetch and select Wilaya and Commune based on geolocation

---
+++ deploily-console/src/lib/features/select-wilaya/selectWilayaInterface.tsx
@@ -0,0 +1,20 @@
+
+export interface WilayaInterface {
+    id: number;
+    code: number;
+    nom: string;
+    nom_maj: string;
+    nom_ar: string;
+}
+
+
+export interface CommuneInterface {
+    id: number;
+    code_48: number;
+    code_58: number;
+    code_5: number;
+    nom: string;
+    nom_maj: string;
+    nom_ar: string | null;
+    code_wil: number;
+}
---
+++ deploily-console/src/lib/features/select-wilaya/selectWilayaThunks.tsx
@@ -0,0 +1,54 @@
+import axiosInstance from "@/app/api/axios-instance";
+import { deploilyApiUrls } from "@/deploilyWebsiteUrls";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+export const fetchWilayaFromPosition = createAsyncThunk(
+  "selectWilaya/fetchWilayaFromPosition",
+  async ({ lat, long }: { lat: number; long: number }, thunkConfig) => {
+    try {
+
+      const response = await axiosInstance.get(`${deploilyApiUrls.GET_WILAYA__URL}lat=${lat}&long=${long}`, {
+        headers: {
+          accept: "application/json",
+          apikey: process.env.NEXT_PUBLIC_API_KEY, // Use your API key if not provided
+        },
+      }
+      );
+
+      if (response.status === 200) {
+        return response.data.data.wilaya;
+      }
+      else {
+        return thunkConfig.rejectWithValue("Wilaya not found");
+
+
+      }
+    } catch (error) {
+      return thunkConfig.rejectWithValue(error);
+    }
+  }
+);
+
+
+export const fetchCommuneFromPosition = createAsyncThunk(
+  "selectWilaya/fetchCommuneFromPosition",
+  async ({ lat, long }: { lat: number; long: number }, thunkConfig) => {
+    try {
+      const response = await axiosInstance.get(`${deploilyApiUrls.GET_COMMUNE_URL}lat=${lat}&long=${long}`, {
+        headers: {
+          accept: "application/json",
+          apikey: process.env.NEXT_PUBLIC_API_KEY, // Use your API key if not provided
+        },
+      }
+      );
+      if (response.status === 200) {
+        return response.data.data.commune;
+      }
+      else {
+        return thunkConfig.rejectWithValue("Failed to fetch commune");
+      }
+    } catch (error) {
+      return thunkConfig.rejectWithValue(error);
+    }
+  }
+);
