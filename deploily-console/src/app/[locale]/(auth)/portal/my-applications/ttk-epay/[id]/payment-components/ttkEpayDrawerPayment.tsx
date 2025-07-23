d56555a9a8abe6c1f92a480b718ba77f47650d8a
Sedjai Bouchra <bouchra.sedjai@transformatek.dz>
2025-07-23 13:51:11 +0000

    Fix drawer ttkEpay design.

--- deploily-console/src/app/[locale]/(auth)/portal/my-applications/ttk-epay/[id]/payment-components/ttkEpayDrawerPayment.tsx
+++ deploily-console/src/app/[locale]/(auth)/portal/my-applications/ttk-epay/[id]/payment-components/ttkEpayDrawerPayment.tsx
@@ -123,99 +123,123 @@ export default function TtkEpayPaymentDrawer({ openDrawer, onClose, subscription
 
                 <Col style={{ padding: 20 }}>
 
-                    <Card style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        height: "100%",
-                        borderColor: theme.token.gray50,
-                        boxShadow: "none",
-                    }}
+                    <Card
+                        style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            height: "100%",
+                            borderColor: theme.token.gray50,
+                            boxShadow: "none",
+                            padding: 16,
+                        }}
                     >
-                        <Row gutter={16} align="top" >
-                            <Col span={14} >  <Typography.Text strong >{tApplications("svc")}</Typography.Text></Col>
-                            <Col span={10} > <Typography.Text >{ttkEpayById?.name}</Typography.Text></Col>
-                        </Row>
-                        <Row gutter={16} align="top" >
-                            <Col span={14} >  <Typography.Text strong >{tApplications("plan")}</Typography.Text></Col>
-                            <Col span={10} > <Typography.Text >{app_service_plan?.plan.name}</Typography.Text></Col>
-                        </Row>
-                        <Row gutter={16} align="top" >
-                            <Col span={14} >  <Typography.Text strong >{tApplications("duration")}</Typography.Text></Col>
-                            <Col span={10} >
-                                <ConfigProvider theme={{
-                                    components: {
-                                        Select: {
-                                            colorBgContainer: theme.token.gray50,
-                                        }
-                                    }
-                                }}>
-                                    <Select
-                                        defaultValue={duration}
-                                        style={{ width: 150, borderRadius: "10px" }}
-                                        onChange={handleChangeDuration}
-                                        dropdownStyle={{
-                                            backgroundColor: theme.token.gray50,
-                                            border: `2px solid ${theme.token.gray100}`,
+                        {[
+                            { label: tApplications("svc"), value: ttkEpayById?.name },
+                            { label: tApplications("plan"), value: app_service_plan?.plan.name },
+                            {
+                                label: tApplications("duration"),
+                                value: (
+                                    <ConfigProvider
+                                        theme={{
+                                            components: {
+                                                Select: {
+                                                    colorBgContainer: theme.token.gray50,
+                                                },
+                                            },
                                         }}
-                                        options={options}
-                                    />
-                                </ConfigProvider>
-                            </Col>
-                        </Row>
-                        <Row gutter={16} align="top" >
-                            <Col span={14} >  <Typography.Text strong >{tApplications("version")}</Typography.Text></Col>
-                            <Col span={10} >
-                                <ConfigProvider theme={{
-                                    components: {
-                                        Select: {
-                                            colorBgContainer: theme.token.gray50,
-                                        }
-                                    }
-                                }}>
-                                    <Select
-                                        defaultValue={typeof selected_version?.id === "number" ? selected_version.id : undefined}
-                                        style={{ width: 150, borderRadius: "10px" }}
-                                        onChange={handleChangeVersion}
-                                        dropdownStyle={{
-                                            backgroundColor: theme.token.gray50,
-                                            border: `2px solid ${theme.token.gray100}`,
+                                    >
+                                        <Select
+                                            defaultValue={duration}
+                                            style={{ width: 150, borderRadius: "10px" }}
+                                            onChange={handleChangeDuration}
+                                            dropdownStyle={{
+                                                backgroundColor: theme.token.gray50,
+                                                border: `2px solid ${theme.token.gray100}`,
+                                            }}
+                                            options={options}
+                                        />
+                                    </ConfigProvider>
+                                ),
+                            },
+                            {
+                                label: tApplications("version"),
+                                value: (
+                                    <ConfigProvider
+                                        theme={{
+                                            components: {
+                                                Select: {
+                                                    colorBgContainer: theme.token.gray50,
+                                                },
+                                            },
                                         }}
-                                        options={optionsVersion}
-                                    />
-                                </ConfigProvider>
-                            </Col>
-                        </Row>
-                        <Row gutter={16} align="top" >
-                            <Col span={14} >  <Typography.Text strong >{tApplications("provider")}</Typography.Text></Col>
-                            <Col span={10} > <Typography.Text >{resource_service_plan?.provider_info?.name}</Typography.Text></Col>
-                        </Row>
-                        <Row gutter={16} align="top" >
-                            <Col span={14} >  <Typography.Text strong >{tApplications("vpsType")}</Typography.Text></Col>
-                            <Col span={10} > <Typography.Text >{resource_service_plan?.service_name ?? ""}</Typography.Text></Col>
-                        </Row>
-                        <Row gutter={16} align="top" >
-                            <Col span={14} >  <Typography.Text strong >{tApplications("resourcePlan")}</Typography.Text></Col>
-                            <Col span={10} > <Typography.Text >{resource_service_plan?.plan_name ?? ""}</Typography.Text></Col>
-                        </Row>
-                        <Row gutter={16} align="top" >
-                            <Col span={14} >  <Typography.Text strong >{tApplications("promoCode")}</Typography.Text></Col>
-                            <Col span={10} >
-
-                                <AppPromoCodeTextField />
-
-                            </Col>
-                        </Row>
-                        <Row gutter={16} align="top" >
-                            <Col span={14} >  <Typography.Text strong >{("totalAmount")}</Typography.Text></Col>
-                            <Col span={10} color="red">
-                                <Typography.Text strong style={{ fontSize: 16, fontWeight: 500, color: theme.token.orange400 }}>
-                                    {Intl.NumberFormat('fr-FR', { useGrouping: true }).format(totalAmount)}
-                                    <span style={{ fontSize: 12, fontWeight: 400 }}>  DZD</span>
-                                </Typography.Text>
-
-                            </Col>
-                        </Row>
+                                    >
+                                        <Select
+                                            defaultValue={
+                                                typeof selected_version?.id === "number"
+                                                    ? selected_version.id
+                                                    : undefined
+                                            }
+                                            style={{ width: 150, borderRadius: "10px" }}
+                                            onChange={handleChangeVersion}
+                                            dropdownStyle={{
+                                                backgroundColor: theme.token.gray50,
+                                                border: `2px solid ${theme.token.gray100}`,
+                                            }}
+                                            options={optionsVersion}
+                                            getPopupContainer={(trigger) => trigger.parentNode}
+                                        />
+                                    </ConfigProvider>
+                                ),
+                            },                              
+                            {
+                                label: tApplications("provider"),
+                                value: resource_service_plan?.provider_info?.name,
+                            },
+                            {
+                                label: tApplications("vpsType"),
+                                value: resource_service_plan?.service_name ?? "",
+                            },
+                            {
+                                label: tApplications("resourcePlan"),
+                                value: resource_service_plan?.plan_name ?? "",
+                            },
+                            {
+                                label: tApplications("promoCode"),
+                                value: <AppPromoCodeTextField />,
+                            },
+                            {
+                                label: tSubscription("totalAmount"),
+                                value: (
+                                    <Typography.Text
+                                        strong
+                                        style={{
+                                            fontSize: 16,
+                                            fontWeight: 500,
+                                            color: theme.token.orange400,
+                                        }}
+                                    >
+                                        {Intl.NumberFormat("fr-FR", { useGrouping: true }).format(
+                                            totalAmount
+                                        )}
+                                        <span style={{ fontSize: 12, fontWeight: 400 }}> DZD</span>
+                                    </Typography.Text>
+                                ),
+                            },
+                        ].map((item, index) => (
+                            <Row
+                                gutter={16}
+                                align="top"
+                                style={{ marginBottom: index === 8 ? 0 : 12 }}
+                                key={index}
+                            >
+                                <Col span={14}>
+                                    <Typography.Text strong>{item.label}</Typography.Text>
+                                </Col>
+                                <Col span={10}>{item.value}</Col>
+                            </Row>
+                        ))}
                     </Card>
+
                     {paymentProfilesList?.count != undefined && paymentProfilesList?.count > 0 && <SelectProfileComponent
                         translations={{ title: tSubscription("selectProfile"), profile: tSubscription("profile"), balance: tSubscription("balance") }}
                         selectedProfile={selectedProfile}
