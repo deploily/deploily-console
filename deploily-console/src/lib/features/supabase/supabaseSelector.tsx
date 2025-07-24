8bf4c2edbd6fa811dd8fcf21ca0caf639f557738
Sedjai Bouchra <bouchra.sedjai@transformatek.dz>
2025-07-24 15:14:23 +0000

    Add Supabase app interfaces, slice, selector, and thunk for fetching app details.

---
+++ deploily-console/src/lib/features/supabase/supabaseSelector.tsx
@@ -0,0 +1,4 @@
+import { useSelector } from "react-redux";
+import { RootState } from "../../store";
+
+export const useSupabaseAppById = () => useSelector((state: RootState) => state.supabaseApp.supabaseAppById);
