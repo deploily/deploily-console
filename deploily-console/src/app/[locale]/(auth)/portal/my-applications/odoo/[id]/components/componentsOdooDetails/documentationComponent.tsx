e64d65a3a5ca4a8653b80f1cb20e7c949122f160
Sedjai Bouchra <bouchra.sedjai@transformatek.dz>
2025-07-29 15:04:14 +0000

    Refactor MyAppDetails to improve button functionality and add HUB_URL constant

--- deploily-console/src/app/[locale]/(auth)/portal/my-applications/odoo/[id]/components/componentsOdooDetails/documentationComponent.tsx
+++ deploily-console/src/app/[locale]/(auth)/portal/my-applications/odoo/[id]/components/componentsOdooDetails/documentationComponent.tsx
@@ -1,57 +1,54 @@
 import { CustomTransparentOrangeButton } from "@/styles/components/buttonStyle";
 import { Col } from "antd";
 import { useI18n, useScopedI18n } from "../../../../../../../../../../locales/client";
+import { HUB_URL } from "@/deploilyWebsiteUrls";
 
 
-export default function DocumentationComponent({ odooAppById, setOpenDrawer }: { odooAppById: any, setOpenDrawer :any}) {
+export default function DocumentationComponent({ odooAppById, setOpenDrawer }: { odooAppById: any, setOpenDrawer: any }) {
     const t = useI18n();
-    
-    const tSubscription = useScopedI18n('subscription');
-    
-    
-      
-  return (
-      <>
 
-                      <Col span={24} style={{
-                          display: "flex",
-                          justifyContent: "end",
-                          alignSelf: "start"
-                      }}>
-                          <CustomTransparentOrangeButton
-                          onClick={() => setOpenDrawer(true)}
-                          >
-                              {t('moreDetails')}
-                          </CustomTransparentOrangeButton>
-                      </Col>
-                      <Col span={24} style={{
-                          display: "flex",
-                          justifyContent: "end",
-                          alignSelf: "start"
-                      }}>
-                          <CustomTransparentOrangeButton
-                              href={odooAppById.service_details.documentation_url ?? "https://docs.deploily.cloud/#/"}
-                              target="_blank"
-                              rel="noopener noreferrer"
+    return (
+        <>
 
-                          >
-                              {t('documentation')}
-                          </CustomTransparentOrangeButton>
-                      </Col>
-                      <Col span={24} style={{
-                          display: "flex",
-                          justifyContent: "end",
-                          alignSelf: "start"
-                      }}>
-                          <CustomTransparentOrangeButton
-                              href={odooAppById.console_url}
-                              target="_blank"
-                              rel="noopener noreferrer"
+            <Col span={24} style={{
+                display: "flex",
+                justifyContent: "end",
+                alignSelf: "start"
+            }}>
+                <CustomTransparentOrangeButton
+                    onClick={() => setOpenDrawer(true)}
+                >
+                    {t('moreDetails')}
+                </CustomTransparentOrangeButton>
+            </Col>
+            <Col span={24} style={{
+                display: "flex",
+                justifyContent: "end",
+                alignSelf: "start"
+            }}>
+                <CustomTransparentOrangeButton
+                    href={odooAppById.service_details.documentation_url ?? "https://docs.deploily.cloud/#/"}
+                    target="_blank"
+                    rel="noopener noreferrer"
 
-                          >
-                              {tSubscription('adminConsole')}
-                          </CustomTransparentOrangeButton>
-                      </Col>
-    </>
-  );
+                >
+                    {t('documentation')}
+                </CustomTransparentOrangeButton>
+            </Col>
+            <Col span={24} style={{
+                display: "flex",
+                justifyContent: "end",
+                alignSelf: "start"
+            }}>
+                <CustomTransparentOrangeButton
+                    href={HUB_URL}
+                    target="_blank"
+                    rel="noopener noreferrer"
+
+                >
+                    {t('supportTicket')}
+                </CustomTransparentOrangeButton>
+            </Col>
+        </>
+    );
 }
