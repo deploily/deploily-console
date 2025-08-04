3cb9fa8fb511a9125364484aa81671a3f87cd09b
Sedjai Bouchra <bouchra.sedjai@transformatek.dz>
2025-08-04 15:44:51 +0000

    Refactor ApplicationDetailsCollapseContainer to enhance specifications rendering with ReactMarkdown.

--- deploily-console/src/app/[locale]/(auth)/portal/application/[id]/containers/applicationDetailsCollapseContainer.tsx
+++ deploily-console/src/app/[locale]/(auth)/portal/application/[id]/containers/applicationDetailsCollapseContainer.tsx
@@ -1,24 +1,33 @@
 'use client';
 import { DetailsCollapse } from 'deploily-ui-components';
 import { useScopedI18n } from '../../../../../../../../locales/client';
+import ReactMarkdown from 'react-markdown';
 
-export default function ApplicationDetailsCollapseContainer({ description, specifications }: { description: string, specifications: string}) {
-    const tApplications = useScopedI18n('applications')
+export default function ApplicationDetailsCollapseContainer({
+    description,
+    specifications,
+}: {
+    description: string;
+    specifications: string;
+}) {
+    const tApplications = useScopedI18n('applications');
+
     return (
         <DetailsCollapse
-            items={
-                [
-                    {
-                        label: tApplications('description'),
-                        children: description
-                    },
-                    {
-                        label: tApplications("features"),
-                        children: specifications,
-                    }
-                ]
-            }
+            items={[
+                {
+                    label: tApplications('description'),
+                    children: description,
+                },
+                {
+                    label: tApplications('features'),
+                    children: (
+                        <div style={{ fontWeight: 600, fontSize: 14 }}>
+                            <ReactMarkdown>{specifications}</ReactMarkdown>
+                        </div>
+                    ),
+                },
+            ]}
         />
-
     );
 }
