d3e4878664635e1d1e56994c1dd1a7bc74b44fd4
kawther boukra <kawther.boukra@transformatek.dz>
2025-08-21 15:03:49 +0000

    enable epay button

--- deploily-console/src/app/[locale]/(auth)/portal/api-services/[id]/components/apiServiceSubscriptionDrawer/components/epayButton.tsx
+++ deploily-console/src/app/[locale]/(auth)/portal/api-services/[id]/components/apiServiceSubscriptionDrawer/components/epayButton.tsx
@@ -7,45 +7,46 @@ import { useEffect, useState } from "react";
 export default function EpayButton({ handleSubscribe }: { handleSubscribe: () => void }) {
 
     const [isPaymentEnabled, setIsPaymentEnabled] = useState<any>(undefined)
-        useEffect(() => {
-            const checkEpaymentPermission = async () => {
-                const paymentEnabled  =await getEpaymentPermission() 
-                setIsPaymentEnabled(paymentEnabled);
+    useEffect(() => {
+        const checkEpaymentPermission = async () => {
+            const paymentEnabled = await getEpaymentPermission()
+            setIsPaymentEnabled(paymentEnabled);
         };
-            checkEpaymentPermission();   
-     
-        }, []);
+        checkEpaymentPermission();
+
+    }, []);
     return (
         <>
-                    <Button
-                        disabled={!isPaymentEnabled}
-                        style={{
-                            color: "#fff",
-                            backgroundColor:
-                                (!isPaymentEnabled)
-                                    ? "#d9d9d9" 
-                                    : theme.token.blue300,
-                            border: "none",
-                            padding: "25px 10px",
-                            display: "flex",
-                            alignItems: "center",
-                            gap: "10px",
-                        }}
-                        icon={
-                            <Image
-                                src="/images/paymentIcon.png"
-                                alt="PAY"
-                                style={{ width: 60, height: 35 }}
-                                preview={false}
-                            />
-                        }
-                        onClick={handleSubscribe}
-                    >
-                      
-                        <span style={{ fontSize: "16px", fontWeight: 600 }}>
-                            PAY
-                        </span>
-                    </Button>
+            <Button
+                //TODO add uncomment this
+                // disabled={!isPaymentEnabled}
+                style={{
+                    color: "#fff",
+                    backgroundColor:
+                        (!isPaymentEnabled)
+                            ? "#d9d9d9"
+                            : theme.token.blue300,
+                    border: "none",
+                    padding: "25px 10px",
+                    display: "flex",
+                    alignItems: "center",
+                    gap: "10px",
+                }}
+                icon={
+                    <Image
+                        src="/images/paymentIcon.png"
+                        alt="PAY"
+                        style={{ width: 60, height: 35 }}
+                        preview={false}
+                    />
+                }
+                onClick={handleSubscribe}
+            >
+
+                <span style={{ fontSize: "16px", fontWeight: 600 }}>
+                    PAY
+                </span>
+            </Button>
         </>
     )
 }
