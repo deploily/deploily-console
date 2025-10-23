9f5b537cdcba161209b5ab599cf092eccb475b0f
bentayeb dounia <dounia.bentayeb@transformatek.dz>
2025-10-23 14:36:07 +0000

    update Docker interface

--- deploily-console/src/lib/features/docker/dockerInterface.tsx
+++ deploily-console/src/lib/features/docker/dockerInterface.tsx
@@ -8,16 +8,21 @@ export interface DockerDeptResponse {
     result: dockerDepInterface[];
 }
 export interface dockerDepInterface {
-    access_url: string ;
-    api_key: string ;
-    custom_paramters: CustomParameter[]; 
-    deployment_error: string ;
+    access_url: string;
+    api_key: string;
+    argocd_url: string;
+    argocd_user_name: string;
+    argocd_password: string;
+    backend_url: string;
+    frontend_url: string;
+    custom_paramters: CustomParameter[];
+    deployment_error: string;
     duration_month: number;
     id: number;
     is_expired: boolean;
-    managed_ressource: ManagedResource ;
+    managed_ressource: ManagedResource;
     managed_ressource_details: Record<string, any>; // empty object possible
-    managed_ressource_id: number ;
+    managed_ressource_id: number;
     name: string;
     phone: string | null;
     price: number;
@@ -25,7 +30,7 @@ export interface dockerDepInterface {
     service_details: ServiceDetails;
     service_plan: ServicePlan;
     service_plan_id: number;
-    start_date: string; 
+    start_date: string;
     status: string;
     total_amount: number;
     get_plan_details: GetPlanDetails
@@ -36,8 +41,8 @@ export interface GetPlanDetails {
     plan: PlanInfo;
     price: number;
     service_plan_id: number;
-    service_plan_type: string; 
-    subscription_category: string; 
+    service_plan_type: string;
+    subscription_category: string;
 }
 
 export interface PlanOption {
@@ -45,7 +50,7 @@ export interface PlanOption {
     icon: string;
     id: number;
     sequence: number | null;
-    type: string; 
+    type: string;
     value: number;
 }
 
@@ -73,13 +78,13 @@ export interface ServiceDetails {
     description: string;
     documentation_url: string;
     id: number;
-    image_service: string ;
+    image_service: string;
     is_eligible: boolean;
     is_published: boolean;
     is_subscribed: boolean;
     name: string;
     price_category: string;
-    sequence: number ;
+    sequence: number;
     service_slug: string;
     service_unity: string;
     short_description: string;
